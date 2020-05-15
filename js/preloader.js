924089afc9327eb535033967d718aeb032798c08
hypaxmda <33388336+hypaxmda@users.noreply.github.com>
2020-05-15 18:18:29 +0200

    1

---
+++ js/preloader.js
@@ -0,0 +1,12 @@
+
+// $(document).ready(function(){
+			// 	$('div#loading').removeAttr('id');
+		// });
+		var preloader = document.getElementById("loader");
+		// window.addEventListener('load', function(){
+		// 	preloader.style.display = 'none';
+		// 	})
+
+		function myFunction(){
+			preloader.style.display = 'none';
+		};
